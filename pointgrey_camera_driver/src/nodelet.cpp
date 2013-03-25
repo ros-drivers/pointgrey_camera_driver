af2a8693853f8876bbae1a4cb6c9d28455beac13
Dave Bradley <dbradley@t1000.(none)>
2013-03-25 17:46:23 -0400

    Updated with correct license and copyright headers for public release.

--- pointgrey_camera_driver/src/nodelet.cpp
+++ pointgrey_camera_driver/src/nodelet.cpp
@@ -1,21 +1,31 @@
-/**This code was developed by the National Robotics Engineering Center (NREC),
-part of Carnegie Mellon University's Robotics Institute.  Its development was
-funded by DARPA under the LS3 program and submitted for public release
-on June 7th, 2012 (granted on <NEWDATE_RELEASE_REPLACE>).*/
+/*
+This code was developed by the National Robotics Engineering Center (NREC), part of the Robotics Institute at Carnegie Mellon University. 
+Its development was funded by DARPA under the LS3 program and submitted for public release on June 7th, 2012. 
+Release was granted on August, 21st 2012 with Distribution Statement "A" (Approved for Public Release, Distribution Unlimited).
+
+This software is released under a BSD license:
+
+Copyright (c) 2012, Carnegie Mellon University. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of the Carnegie Mellon University nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/ 
+
 
 
 /**
    @file nodelet.cpp
-   @author Chad Rockey (chadrockey@nrec.ri.cmu.edu)
+   @author Chad Rockey
    @date July 13, 2011
    @brief ROS nodelet for the Point Grey Chameleon Camera
    
    @attention Copyright (C) 2011
    @attention National Robotics Engineering Center
    @attention Carnegie Mellon University
-   @attention All rights reserved
-   @attention Carnegie Mellon Proprietary
-   @attention Government Purpose Rights
 */
 
 // ROS and associated nodelet interface and PLUGINLIB declaration header
