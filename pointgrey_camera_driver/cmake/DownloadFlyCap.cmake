aa66ca886b724978c34a14383f2a04e3c9a083c8
tongtybj <chou@jsk.imi.i.u-tokyo.ac.jp>
2017-04-09 07:36:46 +0900

    Add the release version for each architecture

--- pointgrey_camera_driver/cmake/DownloadFlyCap.cmake
+++ pointgrey_camera_driver/cmake/DownloadFlyCap.cmake
@@ -3,13 +3,18 @@ function(download_flycap POINTGREY_LIB_VAR POINTGREY_INCLUDE_DIR_VAR)
     message(FATAL_ERROR "Downloading libflycapture for non-linux systems not supported")
   endif()
 
+  # set architecture
   include(cmake/TargetArch.cmake)
+  # set release
+  execute_process(COMMAND lsb_release -c COMMAND awk "{print $2}" OUTPUT_VARIABLE CODE_NAME)
+  #message(WARNING ${CODE_NAME})
+
   target_architecture(POINTGREY_ARCH)
 
   set(POINTGREY_LIB "${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_LIB_DESTINATION}/libflycapture.so.2")
   set(DOWNLOAD_SCRIPT "${PROJECT_SOURCE_DIR}/cmake/download_flycap")
   execute_process(
-    COMMAND ${DOWNLOAD_SCRIPT} ${POINTGREY_ARCH} ${POINTGREY_LIB}
+    COMMAND ${DOWNLOAD_SCRIPT} ${POINTGREY_ARCH} ${CODE_NAME} ${POINTGREY_LIB}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
 
   set(${POINTGREY_LIB_VAR} ${POINTGREY_LIB} PARENT_SCOPE)
